8f41c62118a20e62fedb41a3de0537b4b4bddabc
Ogun Acik <acikogun@gmail.com>
2020-04-06 09:35:21 +0300

    Remove brace expansion since sh doesn't support it

--- builder_scripts/build.sh
+++ builder_scripts/build.sh
@@ -3,8 +3,10 @@
 # Wrapper script of build_iso.sh
 
 set -e
+set -x
 
 rm -rf /tmp/nuclewall
-rm -rf /home/nuclewall/{src,nucle_gui}
+rm -rf /home/nuclewall/src
+rm -rf /home/nuclewall/nucle_gui
 
 ./build_iso.sh
